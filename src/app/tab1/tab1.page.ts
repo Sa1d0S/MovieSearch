b8d9fad7611302703c2f863c93593ca315774117
SaidOnly <m_prazan@utb.cz>
2025-01-22 14:05:43 +0100

    add movie search functionality to Tab 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +8,20 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
+  movies: any[] = [];
+  query: string = '';
+  private apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=7a0e64a1';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  findMovies() {
+    const query = this.query.trim();
+    if (query.length > 2) {
+      this.http.get(`${this.apiUrl}&s=${query}`).subscribe((res: any) => {
+        this.movies = res.Search || [];
+      });
+    } else {
+      this.movies = [];
+    }
+  }
 }
