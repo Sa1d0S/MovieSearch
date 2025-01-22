04e0887775c67ecd4e80f8173cb7b615a1600759
SaidOnly <m_prazan@utb.cz>
2025-01-22 13:12:20 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MovieSearch',
+  webDir: 'www'
+};
+
+export default config;
